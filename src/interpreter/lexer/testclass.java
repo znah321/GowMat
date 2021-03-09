47edc2161f1308f8134170e86c133fef196192bd
znah321 <541313324@qq.com>
2021-03-09 21:56:46 +0800

    测试类

--- src/interpreter/lexer/testclass.java
+++ src/interpreter/lexer/testclass.java
@@ -1,12 +1,29 @@
-package interpreter;
+package interpreter.lexer;
 
-import com.sun.xml.internal.ws.api.ha.StickyFeature;
+import interpreter.parser.Parser;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class testclass {
     public static void main(String[] args) {
+
         String regex = "a";
         System.out.println(Pattern.matches("\".*", "\"aaa "));
+        Lexer lexer = new Lexer("code");
+        lexer.analyze();
+//        lexer.save_result();
+        Parser parser = new Parser(lexer);
+        parser.parse();
+
+         /*
+        String str = "B = C([1:2:5],[2,3]) + [1,2,3;4,5,6;7,8,9].^2 + sum(A,1)";
+        Pattern p = Pattern.compile("\\[(([+-]?\\d+(\\.\\d+)?)([\\s]*[,]?[\\s]*[+-]?\\d+(\\.\\d+)?)*)([\\s]*;[\\s]*([+-]?\\d+(\\.\\d+)?)([\\s]*[,]?[\\s]*[+-]?\\d+(\\.\\d+)?)*)*\\]");
+        Matcher m = p.matcher(str);
+        m.find();
+        System.out.println(m.group(0));
+        System.out.println(m.group(1));
+
+          */
     }
 }
