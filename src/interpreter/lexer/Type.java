9f67c4be1dcd65ace690fe25ae87387fd519e310
znah321 <541313324@qq.com>
2021-03-09 21:56:29 +0800

    Token类型

--- src/interpreter/lexer/Type.java
+++ src/interpreter/lexer/Type.java
@@ -1,4 +1,14 @@
 package interpreter.lexer;
 
-public enum Type {
+enum Type {
+    identifier, // 标识符：变量名之类的 a ab _aaa
+    key, // 关键字：内置的函数名，以及if、else等
+    operator, // 运算符：+ - * / % ^ .^ ./ .* ( ) [ ] , > < = >= <= || && | & :
+    str_literals, // 字符串字面量 "hello gowmat"
+    num_literals, // 数字字面量 1 2 34.5
+    mat_literals, // 矩阵字面量
+    undefined, // 未定义
+    separator, // 分隔符（逗号，分号）
+    end_of_line, // 行末尾
+    end_of_file; // 文件末尾
 }
