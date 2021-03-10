ea31c573c0d09f15b7ec6ac6077011995e273376
znah321 <541313324@qq.com>
2021-03-10 20:06:34 +0800

    测试类

--- src/interpreter/lexer/testclass.java
+++ src/interpreter/lexer/testclass.java
@@ -1,5 +1,6 @@
 package interpreter.lexer;
 
+import GowMat.Matrix;
 import interpreter.parser.Parser;
 
 import java.util.regex.Matcher;
@@ -8,15 +9,31 @@ import java.util.regex.Pattern;
 public class testclass {
     public static void main(String[] args) {
 
+        /*
         String regex = "a";
         System.out.println(Pattern.matches("\".*", "\"aaa "));
+
+         */
+
+
         Lexer lexer = new Lexer("code");
         lexer.analyze();
-//        lexer.save_result();
+        lexer.save_result();
         Parser parser = new Parser(lexer);
         parser.parse();
 
-         /*
+
+//        Matrix m = new Matrix("[]");
+//        System.out.println(m.toToken());
+
+
+        /*
+        System.out.println(Pattern.matches("print|sum", "sum"));
+
+
+         */
+
+        /*
         String str = "B = C([1:2:5],[2,3]) + [1,2,3;4,5,6;7,8,9].^2 + sum(A,1)";
         Pattern p = Pattern.compile("\\[(([+-]?\\d+(\\.\\d+)?)([\\s]*[,]?[\\s]*[+-]?\\d+(\\.\\d+)?)*)([\\s]*;[\\s]*([+-]?\\d+(\\.\\d+)?)([\\s]*[,]?[\\s]*[+-]?\\d+(\\.\\d+)?)*)*\\]");
         Matcher m = p.matcher(str);
@@ -24,6 +41,6 @@ public class testclass {
         System.out.println(m.group(0));
         System.out.println(m.group(1));
 
-          */
+         */
     }
 }
