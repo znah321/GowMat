a0d014b12e20e717c3d40638b2c6a83017d78f15
znah321 <541313324@qq.com>
2021-03-10 20:06:21 +0800

    修改一点细节

--- src/interpreter/lexer/Type.java
+++ src/interpreter/lexer/Type.java
@@ -1,12 +1,15 @@
 package interpreter.lexer;
 
-enum Type {
+public enum Type {
     identifier, // 标识符：变量名之类的 a ab _aaa
     key, // 关键字：内置的函数名，以及if、else等
     operator, // 运算符：+ - * / % ^ .^ ./ .* ( ) [ ] , > < = >= <= || && | & :
     str_literals, // 字符串字面量 "hello gowmat"
     num_literals, // 数字字面量 1 2 34.5
     mat_literals, // 矩阵字面量
+    num,
+    mat,
+    string,
     undefined, // 未定义
     separator, // 分隔符（逗号，分号）
     end_of_line, // 行末尾
