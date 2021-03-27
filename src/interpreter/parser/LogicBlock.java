79e4d78a47afebb588f787ba0655f4fdcdf9ab4c
znah321 <541313324@qq.com>
2021-03-27 22:12:36 +0800

    first commit

---
+++ src/interpreter/parser/LogicBlock.java
@@ -0,0 +1,7 @@
+package interpreter.parser;
+
+public class LogicBlock extends SubParser {
+    public LogicBlock(Parser mainParser) {
+        super(mainParser);
+    }
+}
---
+++ src/interpreter/parser/LoopBlock.java
@@ -0,0 +1,7 @@
+package interpreter.parser;
+
+public class LoopBlock extends SubParser{
+    public LoopBlock(Parser mainParser) {
+        super(mainParser);
+    }
+}
